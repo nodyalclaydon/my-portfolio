f6bcedf14c9fe0013ca58da9b209820d1194180c
nodyalclaydon <72510779+nodyalclaydon@users.noreply.github.com>
2021-04-22 10:26:22 -0500

    feature: adding project section

---
+++ scroll.js
@@ -0,0 +1,5 @@
+const projectCard = document.getElementsByClassName("project-card")
+const projectCarousel = [projectCard[3], projectCard[0], projectCard[1], projectCard[2]]
+
+// event listener for mouseover, then function that changes order of array on scroll/swipe
+// add className to second position... first and third always visible
