3e2017d0533c723810f1993f0da2cf9399790ccd
nodyalclaydon <72510779+nodyalclaydon@users.noreply.github.com>
2021-04-30 10:54:10 -0500

    featuer: added content areas

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
+// --------Show Menu--------
 const menuBtn = document.querySelector(".menu-btn")
 const navBar = document.querySelector(".navbar")
 const menuItem = document.querySelectorAll(".menu-item")
 
-menuBtn.addEventListener("click", function() {
+menuBtn.addEventListener("click", showMenu)
+
+menuItem.forEach(item => {
+    item.addEventListener("click", showMenu)
+})
+
+function showMenu() {
     navBar.classList.toggle("show-menu")
     menuItem.forEach(item => {
         item.classList.toggle("show-menu")
     })
-})
+}
+
+// -------- Logo Animation --------
+
+const techLogos = document.querySelectorAll(".tech-logos")
+let i = 0
+
+setInterval(cycleLogos, 1500)
+
+function cycleLogos() {
+    if (i === 0) {
+        techLogos[techLogos.length - 1].classList.remove("display")
+    } else {techLogos[i-1].classList.remove("display")}
+
+    techLogos[i].classList.add("display")
+
+    if (i === techLogos.length - 1) {
+        i = 0
+    } else { i++ }
+}
 
 // const icons = document.querySelectorAll(".icon")
 // const aboutLines = document.querySelectorAll(".about-text")
